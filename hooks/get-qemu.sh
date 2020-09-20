5ad3c319cb10523baf5bff06434512d803582a99
Dave Conroy <dave@tiredofit.ca>
2020-09-20 06:50:07 -0700

    More Multiarch

--- hooks/get-qemu.sh
+++ hooks/get-qemu.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # NOTE: this url will change regularly because it's unstable
-PACKAGE=http://ftp.de.debian.org/debian/pool/main/q/qemu/qemu-user-static_4.2-2_amd64.deb
+PACKAGE=http://ftp.us.debian.org/debian/pool/main/q/qemu/$(lynx -listonly -nonumbers -dump http://ftp.us.debian.org/debian/pool/main/q/qemu/ | grep -o qemu-user-static_.*_amd64.deb | sed "s/%2B/+/g " | tail -n 1)
 
 mkdir tmp/
 cd tmp/
@@ -11,5 +11,6 @@ curl $PACKAGE -o $(basename ${PACKAGE})
 dpkg-deb -X $(basename ${PACKAGE}) .
 cp usr/bin/qemu-aarch64-static ..
 cp usr/bin/qemu-arm-static ..
+cp usr/bin/qemu-ppc64le-static ..
 cd ..
 rm -rf tmp
